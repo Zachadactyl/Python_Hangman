abe6444f4bea6423023b2d10ddf4b3c34122a393
Slasher6157 <89289754+Zachadactyl@users.noreply.github.com>
2025-10-27 09:28:06 -0700

    Added basic var and play loop
    Added proper error handling and distributed workload to project needs.

--- Hangman-Main.py
+++ Hangman-Main.py
@@ -1,3 +1,17 @@
 #Hangman Project - Cole, Zachary, and Jace - Started Oct 27, 2025
 
-#Needs: Word bank, Selector, Letter input calculator, Functions, (GUI?), File I/O for saving, 
+#Needs: Word bank, Selector, Letter input calculator, Functions, (GUI?), File I/O for saving, Proper Error Handling, Distributed Workload
+
+import turtle
+import random
+
+
+WillPlay = input("Welcome to hangman! Press |s| to start or |q| to quit.")
+
+if WillPlay.lower() != "q":
+    print("testbox")
+    WillPlay = input("Welcome to hangman! Press |s| to start.")
+else:
+    WillPlay.lower() == "q"
+
+
